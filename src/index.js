2e0bddae258328c396f2b480c49444ee25cc48a3
saniok333 <alex.o.csc@gmail.com>
2022-07-20 22:45:49 +0300

    Connecting to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb+srv://admin:passwordpassword@cluster0.vysdq.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
