a5a74613aea94f6f3eaeee70c64e8820612f7dff
saniok333 <alex.o.csc@gmail.com>
2022-07-21 22:50:40 +0300

    Getting tracks implementation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+
 const User = mongoose.model('User');
 
 const router = express.Router();
